12f793711c15f7399a66affda5af1e0bd8f14e23
Sherrie Jones <sj24kt@comcast.net>
2015-01-20 08:24:54 -0700

    initial commit

---
+++ MCMS/MagicalCreature.h
@@ -0,0 +1,13 @@
+//
+//  MagicalCreature.h
+//  MCMS
+//
+//  Created by Sherrie Jones on 1/20/15.
+//  Copyright (c) 2015 Sherrie Jones. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MagicalCreature : NSObject
+
+@end
---
+++ MCMS/MagicalCreature.m
@@ -0,0 +1,13 @@
+//
+//  MagicalCreature.m
+//  MCMS
+//
+//  Created by Sherrie Jones on 1/20/15.
+//  Copyright (c) 2015 Sherrie Jones. All rights reserved.
+//
+
+#import "MagicalCreature.h"
+
+@implementation MagicalCreature
+
+@end
